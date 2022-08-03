79a25ac3eff368f069b5d5969df45b75921a6178
Alexis LOURS <alexislours@protonmail.com>
2022-08-03 19:51:34 +0200

    feat: build with vite

--- build.bash
+++ build.bash
@@ -5,3 +5,5 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 ~/.deno/bin/deno run --allow-all rss-fetcher.ts
 VITE_BUILD_TIME=$(date +%s)
 ~/.deno/bin/deno run --allow-all rss-builder.ts
+
+npm run build
