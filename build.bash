51e1fcfaf5e3b7c1b739409345264eeabec2535f
Alexis LOURS <alexislours@protonmail.com>
2022-08-04 09:55:52 +0200

    fix: check for pipeline fail

--- build.bash
+++ build.bash
@@ -2,9 +2,9 @@
 
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-~/.deno/bin/deno run --allow-all rss-fetcher.ts
-VITE_BUILD_TIME=$(date +%s)
-echo "VITE_BUILD_TIME=$VITE_BUILD_TIME" > .env
+~/.deno/bin/deno run --allow-all rss-fetcher.ts &&
 ~/.deno/bin/deno run --allow-all rss-builder.ts
 
+VITE_BUILD_TIME=$(date +%s)
+echo "VITE_BUILD_TIME=$VITE_BUILD_TIME" > .env
 npm run build
