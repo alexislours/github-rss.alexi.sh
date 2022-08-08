5dc1cbe68887ff08a9c020dc3fba8607bb6aad60
Alexis LOURS <alexislours@protonmail.com>
2022-08-08 13:32:11 +0200

    feat: add search bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,11 @@
     import Card from "../components/Card.svelte";
     import {LANGUAGES} from "../app.constants";
 
+    let filteredLanguages = LANGUAGES;
+    const filterFeeds = (e) => {
+        const v = e.target.value;
+        filteredLanguages = LANGUAGES.filter(lang => lang.label.toLowerCase().includes(v.toLowerCase()));
+    }
     let buildTime = new Date(+import.meta.env.VITE_BUILD_TIME * 1000);
 </script>
 
@@ -11,12 +16,14 @@
 </header>
 
 <main class="flex flex-row justify-center mt-10 mb-16 flex-wrap gap-6">
-    <div class="w-full flex justify-center">
+    <div class="w-full flex flex-col items-center">
         <p>Last build:
             <span class="tooltip" data-tip="{buildTime.toISOString()}">{buildTime.toLocaleString()}</span>
         </p>
+        <input type="text" placeholder="Search feeds..." class="input input-bordered w-full max-w-xs"
+               on:keyup={filterFeeds}/>
     </div>
-    {#each LANGUAGES as lang}
+    {#each filteredLanguages as lang}
         <Card id={lang.id} label={lang.label}></Card>
     {/each}
 </main>
